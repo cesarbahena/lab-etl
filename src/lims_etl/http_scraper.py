51031c71a4fef66157b731898fdcea43589b57fd
César Bahena <cabahenac@gmail.com>
2019-08-13 10:21:32 -0500

    feat(http): implement HTTP-based scraper with WebForms state management

--- src/lims_etl/http_scraper.py
+++ src/lims_etl/http_scraper.py
@@ -4,13 +4,13 @@ HTTP-based LIMS scraper using requests instead of Selenium.
 This scraper handles ASP.NET WebForms state management directly:
 - Extracts __VIEWSTATE and __EVENTVALIDATION from responses
 - Includes state in every POST request
-- Handles pagination via POST requests
+- Handles pagination via query parameters
 """
 
 import requests
 from bs4 import BeautifulSoup
 import logging
-from typing import Dict, List, Optional, Tuple
+from typing import Dict, List, Optional
 from datetime import datetime
 
 log = logging.getLogger(__name__)
@@ -29,25 +29,28 @@ class WebFormsStateManager:
         '__LASTFOCUS'
     ]
     
+    # ASP.NET Core Antiforgery token
+    ANTIFORGERY_TOKEN = '__RequestVerificationToken'
+    
     @staticmethod
     def extract_state(html: str) -> Dict[str, str]:
         """Extract all hidden form fields from HTML."""
-        soup = BeautifulSoup(html, 'lxml')
+        soup = BeautifulSoup(html, 'html.parser')
         state = {}
         
         for inp in soup.find_all('input', type='hidden'):
             name = inp.get('name')
             value = inp.get('value', '')
-            if name:
+            if name and name.startswith('__'):
                 state[name] = value
         
         return state
     
     @staticmethod
-    def find_field(html: str, field_id: str) -> Optional[str]:
-        """Find a specific field by ID."""
+    def find_field(html: str, field_name: str) -> Optional[str]:
+        """Find a specific field by name."""
         soup = BeautifulSoup(html, 'lxml')
-        inp = soup.find('input', {'id': field_id})
+        inp = soup.find('input', {'name': field_name})
         return inp.get('value') if inp else None
 
 
@@ -82,16 +85,16 @@ class HTTPScraper:
         """Update internal state from HTML response."""
         self.state = WebFormsStateManager.extract_state(html)
     
-    def _post(self, path: str, data: Dict = None) -> requests.Response:
+    def _post(self, path: str, data: Dict = None, allow_redirects: bool = True) -> requests.Response:
         """Make a POST request with current state."""
         url = f"{self.base_url}{path}"
         
-        # Include current state in POST data
-        post_data = {**self.state}
+        # Build POST data with current state
+        post_data = {k: v for k, v in self.state.items() if v}
         if data:
             post_data.update(data)
         
-        response = self.session.post(url, data=post_data, allow_redirects=True)
+        response = self.session.post(url, data=post_data, allow_redirects=allow_redirects)
         self._update_state(response.text)
         
         return response
@@ -110,68 +113,120 @@ class HTTPScraper:
         
         Returns True if authentication successful.
         """
-        log.info(f"Attempting login to {self.base_url}/Login.aspx")
+        log.info(f"Attempting login to {self.base_url}/Login")
         
         # Step 1: GET login page to get initial state
-        response = self._get('/Login.aspx')
+        response = self._get('/Login')
         
         if response.status_code != 200:
             log.error(f"Login page request failed: {response.status_code}")
             return False
         
         # Step 2: Extract state and POST credentials
+        # Field names match the mock: Login1_UserName, Login1_Password, etc.
         login_data = {
-            'Login1$UserName': self.username,
-            'Login1$Password': self.password,
-            'Login1$LoginButton': 'Aceptar'
+            'Login1_UserName': self.username,
+            'Login1_Password': self.password,
+            'Login1_LoginButton': 'Aceptar'
         }
         
-        response = self._post('/Login.aspx', login_data)
+        response = self._post('/Login', login_data, allow_redirects=True)
         
         # Check if login was successful (redirect to consulta page)
-        if '/ConsultaOrdenTrabajo' in response.url or response.status_code == 200:
+        if response.status_code == 302 or '/Consulta' in response.url:
             self.authenticated = True
             log.info("Login successful")
+            return True
+        
+        # Also check if we're on Consulta page now
+        if '/Consulta' in response.text or 'grdConsultaOT' in response.text:
+            self.authenticated = True
+            log.info("Login successful (via content check)")
             return True
         
         log.error("Login failed - check credentials")
         return False
     
-    def navigate_to_consulta(self, client_id: int = 101) -> bool:
+    def get_consulta(self, page: int = 1) -> requests.Response:
         """
-        Navigate to consultation page for a specific client.
-        """
-        log.info(f"Navigating to consulta for client {client_id}")
+        GET the consulta page.
         
-        # If not authenticated, login first
+        Args:
+            page: Page number (1-indexed).
+            
+        Returns:
+            Response object if successful, None otherwise.
+        """
         if not self.authenticated:
             if not self.login():
-                return False
+                return None
         
-        # GET the consulta page
-        response = self._get('/FasePreAnalitica/ConsultaOrdenTrabajo.aspx', 
-                           params={'client': client_id})
+        params = {'page': page} if page > 1 else None
+        response = self._get('/Consulta', params)
         
-        return response.status_code == 200
+        if response.status_code != 200:
+            log.error(f"Consulta request failed: {response.status_code}")
+            return None
+            
+        return response
     
     def search_client(self, client_id: int) -> bool:
         """
         Search for a specific client on the consulta page.
+        
+        Args:
+            client_id: Client ID to search for.
+            
+        Returns:
+            True if search performed successfully.
         """
         search_data = {
-            'ctl00$ContentMasterPage$txtcliente': str(client_id),
-            'ctl00$ContentMasterPage$btnBuscar': 'Buscar'
+            'ctl00_ContentMasterPage_txtcliente': str(client_id),
+            'ctl00_ContentMasterPage_btnBuscar': 'Buscar'
         }
         
-        response = self._post('/FasePreAnalitica/ConsultaOrdenTrabajo.aspx', search_data)
-        
+        response = self._post('/Consulta', search_data)
         return response.status_code == 200
     
-    def parse_grid_row(self, row_element) -> Dict[str, str]:
-        """Parse a single row from the results grid."""
-        data = {}
+    def parse_current_page(self, html: str = None) -> List[Dict[str, str]]:
+        """
+        Parse all data rows from the current page.
         
-        # Map of element IDs to column names (matches LIMS selectors)
+        Returns list of sample records.
+        """
+        soup = BeautifulSoup(html, 'html.parser')
+        records = []
+        
+        # Find the grid table by ID pattern
+        grid = soup.find('table', {'id': lambda x: x and 'grdConsultaOT' in x})
+        if not grid:
+            log.warning("Grid table not found in response")
+            return records
+        
+        # Get all rows in tbody
+        tbody = grid.find('tbody')
+        if not tbody:
+            log.warning("Grid tbody not found")
+            return records
+        
+        # Parse each data row
+        for row in tbody.find_all('tr'):
+            # Skip pagination row (has colspan)
+            if row.find('td', {'colspan': '13'}):
+                continue
+            
+            row_data = self._parse_row(row)
+            if row_data:
+                records.append(row_data)
+        
+        log.info(f"Parsed {len(records)} records from current page")
+        return records
+    
+    def _parse_row(self, row_element) -> Optional[Dict[str, str]]:
+        """Parse a single row from the grid."""
+        row_data = {}
+        
+        # Map of element ID suffix to column name
         column_map = {
             'lblFechaGrd': 'CreatedAt',
             'lblFechaRecep': 'ReceivedAt',
@@ -190,142 +245,78 @@ class HTTPScraper:
         
         for span in row_element.find_all('span'):
             element_id = span.get('id', '')
+            text = span.get_text(strip=True)
             
-            # Extract row number and column name from ID
-            # Format: ctl00_ContentMasterPage_grdConsultaOT_ctl02_lblFechaGrd
+            # Match element ID suffix
             for suffix, col_name in column_map.items():
                 if element_id.endswith(suffix):
-                    data[col_name] = span.get_text(strip=True)
+                    row_data[col_name] = text
                     break
         
-        return data
+        # Only return if we have at least a Folio
+        return row_data if row_data.get('Folio') else None
     
-    def parse_current_page(self, html: str = None) -> List[Dict[str, str]]:
+    def get_samples_page(self, page: int = 1) -> List[Dict[str, str]]:
         """
-        Parse all data rows from the current page.
+        Get all samples from a specific page.
         
-        Returns list of sample records.
-        """
-        soup = BeautifulSoup(html or self.session.last_response.text, 'lxml')
-        records = []
-        
-        # Find the grid table
-        grid = soup.find('table')
-        if not grid:
-            log.warning("Grid table not found in response")
-            return records
-        
-        # Parse each data row (skip header row)
-        for row in grid.find_all('tr')[1:]:
-            row_data = {}
+        Args:
+            page: Page number (1-indexed).
             
-            for span in row.find_all('span'):
-                element_id = span.get('id', '')
-                text = span.get_text(strip=True)
-                
-                # Map element IDs to column names (matches mock format)
-                # Format: grd_ctl02_lblFechaGrd
-                if 'lblFechaGrd' in element_id:
-                    row_data['CreatedAt'] = text
-                elif 'lblFechaRecep' in element_id:
-                    row_data['ReceivedAt'] = text
-                elif 'lblFolioGrd' in element_id:
-                    row_data['Folio'] = text
-                elif 'lblClienteGrd' in element_id:
-                    row_data['ClientId'] = text
-                elif 'lblPacienteGrd' in element_id:
-                    row_data['PatientId'] = text
-                elif 'lblEstPerGrd' in element_id:
-                    row_data['ExamId'] = text
-                elif 'Label1' in element_id:
-                    row_data['ExamName'] = text
-                elif 'lblFecCapRes' in element_id:
-                    row_data['ProcessedAt'] = text
-                elif 'lblFecLibera' in element_id:
-                    row_data['ValidatedAt'] = text
-                elif 'lblSucProc' in element_id:
-                    row_data['Location'] = text
-                elif 'lblMaquilador' in element_id:
-                    row_data['Outsourcer'] = text
-                elif 'Label3' in element_id:
-                    row_data['Priority'] = text
-                elif 'lblFecNac' in element_id:
-                    row_data['BirthDate'] = text
+        Returns:
+            List of sample records.
+        """
+        response = self.get_consulta(page)
+        if not response:
+            return []
+        
+        return self.parse_current_page(response.text)
+    
+    def scrape_all(self, client_id: int = None) -> List[Dict[str, str]]:
+        """
+        Scrape all sample data for the configured client.
+        
+        Args:
+            client_id: Optional client ID to search for.
             
-            if row_data:
-                records.append(row_data)
-        
-        log.info(f"Parsed {len(records)} records from current page")
-        return records
-    
-    def go_to_next_page(self) -> bool:
-        """
-        Navigate to next page of results.
-        
-        Returns True if navigation successful, False if no more pages.
-        """
-        soup = BeautifulSoup(self.session.last_response.text, 'lxml')
-        
-        # Find pagination links
-        pagination = soup.find('div', class_='pagination')
-        if not pagination:
-            log.warning("Pagination element not found")
-            return False
-        
-        # Find next page link
-        current_page_text = f"{self.current_page + 1}"
-        for link in pagination.find_all('a'):
-            if link.get_text(strip=True) == current_page_text:
-                # Click the next page link
-                next_data = {
-                    '__EVENTTARGET': '',  # Would be set by pagination control
-                    '__EVENTARGUMENT': ''
-                }
-                # For this mock, we'll use query parameter
-                next_url = f"{self.base_url}/FasePreAnalitica/ConsultaOrdenTrabajo.aspx?page={self.current_page + 1}"
-                response = self.session.get(next_url)
-                self._update_state(response.text)
-                
-                if response.status_code == 200:
-                    self.current_page += 1
-                    return True
-        
-        log.info("No more pages available")
-        return False
-    
-    def scrape_client(self, client_id: int) -> List[Dict[str, str]]:
-        """
-        Scrape all sample data for a client.
-        
-        Returns list of all sample records.
+        Returns:
+            List of all sample records.
         """
         all_records = []
         
-        # Navigate to client
-        if not self.navigate_to_consulta(client_id):
-            log.error(f"Failed to navigate to client {client_id}")
-            return all_records
+        # Login if needed
+        if not self.authenticated:
+            if not self.login():
+                log.error("Failed to login")
+                return all_records
         
-        # Search for specific client
-        self.search_client(client_id)
+        # Search for client if specified
+        if client_id:
+            self.search_client(client_id)
         
-        # Scrape all pages
+        # Scrape first page
+        page = 1
         while True:
-            records = self.parse_current_page()
-            all_records.extend(records)
+            records = self.get_samples_page(page)
+            if not records:
+                break
             
-            if not self.go_to_next_page():
+            all_records.extend(records)
+            page += 1
+            
+            # Safety limit (mock has 10 pages)
+            if page > 100:
                 break
         
-        log.info(f"Scraped {len(all_records)} total records for client {client_id}")
+        log.info(f"Scraped {len(all_records)} total records")
         return all_records
 
 
 def main():
-    """Test the HTTP scraper."""
+    """Test the HTTP scraper against the mock server."""
     import os
     
-    base_url = os.getenv('LIMS_BASE_URL', 'http://localhost:5000')
+    base_url = os.getenv('LIMS_BASE_URL', 'http://localhost:5080')
     
     scraper = HTTPScraper(base_url)
     
@@ -334,12 +325,18 @@ def main():
         print("Login failed!")
         return
     
-    # Scrape client 101
-    records = scraper.scrape_client(101)
+    # Get first page
+    records = scraper.get_samples_page(1)
     
-    print(f"\nTotal records: {len(records)}")
+    print(f"\nTotal records on page 1: {len(records)}")
     if records:
         print(f"Sample record: {records[0]}")
+        
+        # Verify field parsing
+        record = records[0]
+        print(f"  Folio: {record.get('Folio')}")
+        print(f"  ClientId: {record.get('ClientId')}")
+        print(f"  ExamName: {record.get('ExamName')}")
 
 
 if __name__ == '__main__':
